091c7b4c7dbd620c63c3afacd96cc6a5336e1b5b
Martin Donadieu <martindonadieu@gmail.com>
2026-06-07 12:23:39 +0200

    ci: deploy example app to capgo

---
+++ example-app/capacitor.config.ts
@@ -0,0 +1,24 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+import pkg from './package.json';
+
+const config: CapacitorConfig = {
+  "appId": "app.capgo.flash",
+  "appName": "Flash Example",
+  "webDir": "dist",
+  "plugins": {
+    "SplashScreen": {
+      "launchAutoHide": false
+    },
+    "CapacitorUpdater": {
+      "appId": "app.capgo.flash",
+      "autoUpdate": true,
+      "autoSplashscreen": true,
+      "directUpdate": "always",
+      "defaultChannel": "production",
+      "version": pkg.version
+    }
+  }
+};
+
+export default config;
